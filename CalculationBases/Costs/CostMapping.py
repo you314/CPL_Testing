d34f93dc04174209ec7415e9d5c73b7334d20103
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-11-10 12:22:25 +0100

    #11 Adapted Cost mapping logic using new CPL_Prep method

--- CalculationBases/Costs/CostMapping.py
+++ CalculationBases/Costs/CostMapping.py
@@ -5,26 +5,29 @@ from os import path
 
 class CostMapping:
 
-    def __init__(self, Contractnr):
+    def __init__(self, contract_nr):
         self.ContractDTO = ContractDTO()
-        self.TariffName = self.ContractDTO.tariff_name(Contractnr=Contractnr)
+        self.TariffName = self.ContractDTO.tariff_name(Contractnr=contract_nr)
         relative_path = "/"
-        csvFilename = "TariffName_CostMapping.csv"
-        pfad = path.dirname(__file__) + relative_path + csvFilename
-        self.reader = FileReader(pfad)
-        self.TN_Index = list(self.reader.readColumnFromCSV("TariffNames", type=str).values()).index(self.TariffName)
+        csv_filename = "TariffName_CostMapping.csv"
+        file_path = path.dirname(__file__) + relative_path + csv_filename
+        self.reader = FileReader(file_path)
+        self.Mapping_dictionary = self.reader.create_mapping_by_key("TariffNames")
 
     def acquisition_cost_group(self):
-        return list(self.reader.readColumnFromCSV("AcquisitionCostGroup", type=str).values())[self.TN_Index]
+        return self.Mapping_dictionary[self.TariffName]["AcquisitionCostGroup"]
 
     def amortization_cost_group(self):
-        return list(self.reader.readColumnFromCSV("AmortizationCostGroup", type=str).values())[self.TN_Index]
+        return self.Mapping_dictionary[self.TariffName]["AmortizationCostGroup"]
 
     def administration_cost_group(self):
-        return list(self.reader.readColumnFromCSV("AdministrationCostGroup", type=str).values())[self.TN_Index]
+        return self.Mapping_dictionary[self.TariffName]["AdministrationCostGroup"]
 
     def unit_cost_group(self):
-        return list(self.reader.readColumnFromCSV("UnitCostGroup", type=str).values())[self.TN_Index]
+        return self.Mapping_dictionary[self.TariffName]["UnitCostGroup"]
 
 
-print("Tariff name is: " + CostMapping(Contractnr=124).acquisition_cost_group())
+print("acquisition_cost_group name is: " + CostMapping(contract_nr=124).acquisition_cost_group())
+print("amortization_cost_group name is: " + CostMapping(contract_nr=124).amortization_cost_group())
+print("administration_cost_group name is: " + CostMapping(contract_nr=124).administration_cost_group())
+print("unit_cost_group name is: " + CostMapping(contract_nr=124).unit_cost_group())
