bcd53feb64f09935a595fc4c5cd7b4f5dc1376ae
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-11-16 19:34:44 +0100

    '#6 Change Flags.py file to match coding standards

--- FLags/Flags.py
+++ FLags/Flags.py
@@ -2,27 +2,25 @@ from CPL_Prep import FileReader
 from os import path
 
 
+class Flags:
 
-class Flags():
-
-
-    def createMatrix(self, Tariffgeneration,Tariff:str): # Todo Change the code to understandable one
+    def create_matrix(self, tariff_generation, tariff:str): # Todo Change the code to understandable one
         relative_path = "/"
-        csvFilename = "formulas used.csv"
-        pfad = path.dirname(__file__) + relative_path + csvFilename
-        reader = FileReader(pfad)
-        NetPremium1 = reader.readColumnFromCSV("tg", type=int)
+        csv_file = "formulas used.csv"
+        csv_path = path.dirname(__file__) + relative_path + csv_file
+        csv_reader = FileReader(csv_path)
+        NetPremium1 = csv_reader.readColumnFromCSV("tg", type=int)
         x0= list(NetPremium1.values())
         rowCount = len(NetPremium1)
-        NetPremium2 = reader.read_row_from_csv(0, type=str)
+        NetPremium2 = csv_reader.read_row_from_csv(0, type=str)
         x1 = list(NetPremium2.keys())
-        indexTG = x0.index(int(Tariffgeneration))
-        indexTariff = x1.index(Tariff)
+        indexTG = x0.index(int(tariff_generation))
+        indexTariff = x1.index(tariff)
 
         colCount = len(NetPremium2)
         mat = []
         for i in range(rowCount):
-            X = reader.read_row_from_csv(i)
+            X = csv_reader.read_row_from_csv(i)
             rowList = []
             for j in range(colCount):
                 # you need to increment through dataList here, like this:
@@ -37,7 +35,7 @@ class Flags():
         csvFilename = "Premium.Flags.csv"
         pfad = path.dirname(__file__) + relative_path + csvFilename
         reader = FileReader(pfad)
-        NetPremium1 = reader.readColumnFromCSV(self.createMatrix(Tariffgeneration=Tariffgeneration,Tariff=Tariff), type=str)
+        NetPremium1 = reader.readColumnFromCSV(self.create_matrix(tariff_generation=Tariffgeneration, tariff=Tariff), type=str)
         FlagsVector= list(NetPremium1)
         return FlagsVector
 
