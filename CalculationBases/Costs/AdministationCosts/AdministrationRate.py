b59fc0b82256b1d110fa663574235102ba14eeac
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-11-16 11:20:15 +0100

    #4 AdministationRate.py - doc strings added

---
+++ CalculationBases/Costs/AdministationCosts/AdministrationRate.py
@@ -0,0 +1,30 @@
+from os import path
+from CPL_Prep import FileReader
+from CalculationBases.Costs.CostMapping import CostMapping
+
+
+class AdministrationRate:
+    """
+    Class used to provide the administration cost rate. Its method relies on a csv.
+    AdministrationRates.csv links the unit cost rate to the admin cost group coming from the class CostMapping.py.
+    """
+
+    def __init__(self, contract_nr):  # ToDo: Logic for CostGroups needs to be included, or be part of the IL
+        """
+        :param int contract_nr: The mapping is done via the contract number, which provides the tariff name
+        """
+        relative_path = "/"
+        csv_filename = "AdministrationCostRates.csv"
+        file_path = path.dirname(__file__) + relative_path + csv_filename
+        self.reader = FileReader(file_path)
+        self.Mapping_dictionary = self.reader.create_mapping_by_key("AdministrationCostGroups")
+        self.CostGroup = CostMapping(contract_nr=contract_nr).administration_cost_group()
+
+    def get_administration_cost_by_name(self, cost_type):
+        """
+        Getting the administration cost rate for a cost type, based on cost group
+        :param cost_type: Needs to match the cost_type name in the csv
+        :return: Administration cost rate
+        """
+        value = self.Mapping_dictionary[self.CostGroup][cost_type]
+        return float(value)
