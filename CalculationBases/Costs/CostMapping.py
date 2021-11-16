ad5bda60a85b2e72f06c249288c5eb40edc36c1d
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-11-16 10:13:25 +0100

    #1 CostMapping.py - doc strings added and variable names updated

--- CalculationBases/Costs/CostMapping.py
+++ CalculationBases/Costs/CostMapping.py
@@ -4,12 +4,20 @@ from os import path
 
 
 class CostMapping:
+    """
+    A class used to provide a tariff name cost group mapping. Its methods rely on two csvs.
+    TariffName_CostMapping.csv links the cost groups to the contract based on the tariff's name.
+    Tariff_CostMapping_granular.csv adds a suffix to the cost group based on contract specific parameters.
+    """
 
     def __init__(self, contract_nr):
-        self.ContractDTO = ContractDTO()
-        self.DefermentPeriod = self.ContractDTO.defermentperiod(Contractnr=contract_nr)
-        self.TariffName = self.ContractDTO.tariff_name(Contractnr=contract_nr)
-        self.isNonContributory = self.ContractDTO.is_non_contributory(Contractnr=contract_nr)
+        """
+        :param int contract_nr: The mapping is done via the contract number, which provides the tariff name
+        """
+        self.contract_dto = ContractDTO()
+        self.deferment_period = self.contract_dto.defermentperiod(Contractnr=contract_nr)
+        self.tariff_name = self.contract_dto.tariff_name(Contractnr=contract_nr)
+        self.is_non_contributory = self.contract_dto.is_non_contributory(Contractnr=contract_nr)
         relative_path = "/"
         csv_cost_mapping = "TariffName_CostMapping.csv"
         csv_cost_mapping_granular = "Tariff_CostMapping_granular.csv"
@@ -19,66 +27,89 @@ class CostMapping:
         self.reader_layer2 = FileReader(file_tariff_name_cost_mapping_granular)
         self.Mapping_dictionary1 = self.reader_layer1.create_mapping_by_key("TariffNames")
         self.Mapping_dictionary2 = self.reader_layer2.create_mapping_by_key("CostGroup")
+        self.acquisition_cost_group_name = self.Mapping_dictionary1[self.tariff_name]["AcquisitionCostGroup"]
+        self.amortization_cost_group_name = self.Mapping_dictionary1[self.tariff_name]["AmortizationCostGroup"]
+        self.administration_cost_group_name = self.Mapping_dictionary1[self.tariff_name]["AdministrationCostGroup"]
+        self.unit_cost_group_name = self.Mapping_dictionary1[self.tariff_name]["UnitCostGroup"]
 
     def acquisition_cost_group(self):
-        self.CostGroup = self.Mapping_dictionary1[self.TariffName]["AcquisitionCostGroup"]
-        if self.CostGroup in self.Mapping_dictionary2.keys():
-            CostGroup_granular = self.Mapping_dictionary2[self.CostGroup][self.acquisition_cost_group_granular()]
-            return self.CostGroup + CostGroup_granular
+        """
+        Determination of the acquisition's cost group
+        :return: Combination of cost groups name and the suffix (optional)
+        """
+        if self.acquisition_cost_group_name in self.Mapping_dictionary2.keys():
+            suffix = self.Mapping_dictionary2[self.acquisition_cost_group_name][self.acquisition_cost_group_granular()]
+            return self.acquisition_cost_group_name + suffix
         else:
-            return self.CostGroup
+            return self.acquisition_cost_group_name
 
     def acquisition_cost_group_granular(self):
-        if self.CostGroup == "AK_4":
-            if self.DefermentPeriod < 55:
+        """
+        Determination of the acquisition's cost group suffix
+        :return: The suffix of the cost group based on hard coded contract features
+        """
+        if self.acquisition_cost_group_name == "AK_4":
+            if self.deferment_period < 55:
                 return "Variant1"
             else:
                 return "Variant2"
-        elif self.CostGroup == "AK_8_Courtagestufe2":
-            if self.DefermentPeriod <= 21:
+        elif self.acquisition_cost_group_name == "AK_8_Courtagestufe2":
+            if self.deferment_period <= 21:
                 return "Variant1"
-            elif 21 < self.DefermentPeriod <= 30:
+            elif 21 < self.deferment_period <= 30:
                 return "Variant2"
             else:
                 return "Variant3"
 
     def amortization_cost_group(self):
-        self.CostGroup = self.Mapping_dictionary1[self.TariffName]["AmortizationCostGroup"]
-        if self.CostGroup in self.Mapping_dictionary2.keys():
-            CostGroup_granular = self.Mapping_dictionary2[self.CostGroup][self.amortization_cost_group_granular()]
-            return self.CostGroup + CostGroup_granular
+        """
+        Determination of the amortization's cost group
+        :return: Combination of cost groups name and the suffix (optional)
+        """
+        if self.amortization_cost_group_name in self.Mapping_dictionary2.keys():
+            suffix = self.Mapping_dictionary2[self.amortization_cost_group_name][self.amortization_cost_group_granular()]
+            return self.amortization_cost_group_name + suffix
         else:
-            return self.CostGroup
+            return self.amortization_cost_group_name
 
     def amortization_cost_group_granular(self):
-        if self.CostGroup == "AMK5":
-            if self.DefermentPeriod <= 21:
+        """
+        Determination of the amortization's cost group suffix
+        :return: The suffix of the cost group based on hard coded contract features
+        """
+        if self.amortization_cost_group_name == "AMK5":
+            if self.deferment_period <= 21:
                 return "Variant1"
-            elif 21 < self.DefermentPeriod <= 30:
+            elif 21 < self.deferment_period <= 30:
                 return "Variant2"
             else:
                 return "Variant3"
 
     def administration_cost_group(self):
-        self.CostGroup = self.Mapping_dictionary1[self.TariffName]["AdministrationCostGroup"]
-        if self.CostGroup in self.Mapping_dictionary2.keys():
-            CostGroup_granular = self.Mapping_dictionary2[self.CostGroup][self.administration_cost_group_granular()]
-            return self.CostGroup + CostGroup_granular
+        """
+        Determination of the administration's cost group
+        :return: Combination of cost groups name and the suffix (optional)
+        """
+        if self.administration_cost_group_name in self.Mapping_dictionary2.keys():
+            suffix = self.Mapping_dictionary2[self.administration_cost_group_name][self.administration_cost_group_granular()]
+            return self.administration_cost_group_name + suffix
         else:
-            return self.CostGroup
+            return self.administration_cost_group_name
 
     def administration_cost_group_granular(self):
-        if self.CostGroup == "LVK11":
-            if self.isNonContributory == 1:
+        """
+        Determination of the administration's cost group suffix
+        :return: The suffix of the cost group based on hard coded contract features
+        """
+        if self.administration_cost_group_name == "LVK11":
+            if self.is_non_contributory == 1:
                 return "Variant1"
             else:
                 return "Variant2"
 
     def unit_cost_group(self):
-        return self.Mapping_dictionary1[self.TariffName]["UnitCostGroup"]
-
-
-print("acquisition_cost_group name is: " + CostMapping(contract_nr=123).acquisition_cost_group())
-print("amortization_cost_group name is: " + CostMapping(contract_nr=124).amortization_cost_group())
-print("administration_cost_group name is: " + CostMapping(contract_nr=124).administration_cost_group())
-print("unit_cost_group name is: " + CostMapping(contract_nr=124).unit_cost_group())
+        """
+        Determination of the unit's cost group
+        :return: Cost group
+        """
+        return self.unit_cost_group_name
