edb5241f3c6e6c750b1dc4ff69dfd50d1dcbd3d5
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-11-17 10:10:01 +0100

    '#13 Tariff.py coding style

--- Tariff.py
+++ Tariff.py
@@ -7,17 +7,16 @@ class Tariff(ContractDTO):
 
     def __init__(self, contract_nr):
         super().__init__(contract_nr=contract_nr)
-        self.Presentvalues = PresentValues(contract_nr=contract_nr)
-        self.Flags = Flags()
-        self.flagsVector = self.Flags.flags_vector(tariff_generation=self.tg(), tariff=self.tariff())
+        self.present_values = PresentValues(contract_nr=contract_nr)
+        self.flags = Flags()
+        self.flags_vector = self.flags.flags_vector(tariff_generation=self.tg(), tariff=self.tariff())
 
-    def NetPremiumRente(self):
-        gamma =0.1
-        netPremium = (1+gamma) * self.flagsVector[1] \
-                     * self.Presentvalues.aeg(g=self.guarantee_time()) * \
-                     self.flagsVector[2] + self.Presentvalues.aegk(g=self.guarantee_time(), k=self.m()) * \
-                     self.flagsVector[3] + self.Presentvalues.n_m_a_x(deferment_period= self.deferment_period(), m=self.m(), age=self.actuarial_age(), birth_date=self.birth_year())
-        return netPremium
+    def net_premium_annuity(self):
+        gamma = 0.1
+        net_premium = (1+gamma) * self.flags_vector[1] * self.present_values.aeg(g=self.guarantee_time()) * \
+            self.flags_vector[2] + self.present_values.aegk(g=self.guarantee_time(), k=self.m()) * \
+            self.flags_vector[3] + self.present_values.n_m_a_x(deferment_period=self.deferment_period(), m=self.m(), age=self.actuarial_age(), birth_date=self.birth_year())
+        return net_premium
 
 
-print(Tariff(contract_nr=1234).NetPremiumRente())
+print(Tariff(contract_nr=1234).net_premium_annuity())
