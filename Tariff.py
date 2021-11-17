477342de0b07b00eccad851700a20fe11a564cda
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-11-17 10:07:37 +0100

    '#12 PresentValues.py coding standards / adapt Tariff.py

--- Tariff.py
+++ Tariff.py
@@ -1,4 +1,4 @@
-from PresentValues import Presentvalues
+from PresentValues import PresentValues
 from Contract import ContractDTO
 from FLags.Flags import Flags
 
@@ -7,7 +7,7 @@ class Tariff(ContractDTO):
 
     def __init__(self, contract_nr):
         super().__init__(contract_nr=contract_nr)
-        self.Presentvalues = Presentvalues(contract_nr=contract_nr)
+        self.Presentvalues = PresentValues(contract_nr=contract_nr)
         self.Flags = Flags()
         self.flagsVector = self.Flags.flags_vector(tariff_generation=self.tg(), tariff=self.tariff())
 
@@ -16,7 +16,7 @@ class Tariff(ContractDTO):
         netPremium = (1+gamma) * self.flagsVector[1] \
                      * self.Presentvalues.aeg(g=self.guarantee_time()) * \
                      self.flagsVector[2] + self.Presentvalues.aegk(g=self.guarantee_time(), k=self.m()) * \
-                     self.flagsVector[3] + self.Presentvalues.n_m_a_x(Defermentperiod= self.deferment_period(), m=self.m(), age=self.actuarial_age(), birthDate=self.birth_year())
+                     self.flagsVector[3] + self.Presentvalues.n_m_a_x(deferment_period= self.deferment_period(), m=self.m(), age=self.actuarial_age(), birth_date=self.birth_year())
         return netPremium
 
 
