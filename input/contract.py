65f64eac7f608eff8ab16f582a5dd28e2eb2e975
ThorstenErdmannSyncier <thorsten.erdmann@syncier.com>
2021-12-17 11:55:56 +0100

    '#13 contract.py type hints

--- input/contract.py
+++ input/contract.py
@@ -6,7 +6,7 @@ from typing import Union
 
 class ContractDTO:
 
-    def __init__(self, contract_nr):
+    def __init__(self, contract_nr: int):
         relative_path = "/"
         csv_filename = "Contract.csv"
         csv_path = path.dirname(__file__) + relative_path + csv_filename
@@ -27,63 +27,63 @@ class ContractDTO:
         self.pension_payment_period_dict = csv_reader.read_column_from_csv("pension_payment_period", type=str)
         self.profession_dict = csv_reader.read_column_from_csv("profession", type=str)
 
-    def contract_nr_auxiliary_function(self):
+    def contract_nr_auxiliary_function(self) -> list[int]:
         contract_nr_list = list(self.contract_nr_dict.values())
         return contract_nr_list
 
-    def contract_index(self):
+    def contract_index(self) -> int:
         index = self.contract_nr_auxiliary_function().index(self.contract_nr)
         return index
 
-    def actuarial_age(self):
+    def actuarial_age(self) -> int:
         age = self.age_dict[self.contract_index()]
         return age
 
-    def tg(self):
+    def tg(self) -> int:
         tg = self.tg_dict[self.contract_index()]
         return tg
 
-    def tariff(self):
+    def tariff(self) -> str:
         tariff = self.tariff_dict[self.contract_index()]
         return tariff
 
-    def tariff_name(self):
+    def tariff_name(self) -> str:
         tariff_name = self.tariff_name_dict[self.contract_index()]
         return tariff_name
 
-    def birth_year(self):
+    def birth_year(self) -> int:
         birth_year = self.birth_year_dict[self.contract_index()]
         return birth_year
 
-    def deferment_period(self):
+    def deferment_period(self) -> int:
         deferment_period = self.deferment_period_dict[self.contract_index()]
         return deferment_period
 
-    def guarantee_time(self):
+    def guarantee_time(self) -> int:
         guarantee_time = self.guarantee_time_dict[self.contract_index()]
         return guarantee_time
 
-    def m(self,):
+    def m(self,) -> int:
         m = self.m_dict[self.contract_index()]
         return m
 
-    def sex(self):
+    def sex(self) -> str:
         sex = self.sex_dict[self.contract_index()]
         return sex
 
-    def profession(self):
+    def profession(self) -> str:
         profession = self.profession_dict[self.contract_index()]
         return profession
 
-    def is_non_contributory(self):
+    def is_non_contributory(self) -> int:
         is_non_contributory = self.is_non_contributory_dict[self.contract_index()]
         return is_non_contributory
 
-    def payment_contributions_frequency(self):
+    def payment_contributions_frequency(self) -> int:
         payment_contributions_frequency = self.payment_contributions_frequency_dict[self.contract_index()]
         return payment_contributions_frequency
 
-    def pension_payment_period(self):
+    def pension_payment_period(self) -> int:
         pension_payment_period = self.pension_payment_period_dict[self.contract_index()]
         return pension_payment_period
 
@@ -127,5 +127,4 @@ class ContractDTO:
     pension_payment_period_dict:  int = None
 
 
-print(ContractDTO(contract_nr=123).contract_nr_auxiliary_function())
-
+print("Available contract numbers: " + str(ContractDTO(contract_nr=123).contract_nr_auxiliary_function()))
